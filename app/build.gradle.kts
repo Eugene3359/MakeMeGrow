a58f746a38cf822e65f0e41685610005fb5a387c
Eugene3359 <alukard3359@gmail.com>
2026-07-15 13:39:38 +0300

    style: adjust taskbar text size and dialog spacing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.scipath.makemegrow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 55
-        versionName = "0.5.0"
+        versionCode = 56
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
