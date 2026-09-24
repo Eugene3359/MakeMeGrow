2604f02ce1f6481d8794cc8efe56e1281f2161b2
Eugene3359 <alukard3359@gmail.com>
2026-09-24 23:24:19 +0300

    feat: display confirm button above keyboard in TaskActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.scipath.makemegrow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 78
-        versionName = "0.7.5"
+        versionCode = 79
+        versionName = "0.7.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
